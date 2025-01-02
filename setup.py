9b8265402e7817634b1bcafaf9038c1e1230de25
Aarsh Mehtani <aarshmehtani01@gmail.com>
2025-01-02 17:57:36 +0530

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Aarsh Mehtani',
+    author_email= 'aarshmehtani01@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
